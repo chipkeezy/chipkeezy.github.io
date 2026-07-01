5fa2254ec261ca22eeece4419c8534df1410565f
chipkeezy <mcchipkeezy@gmail.com>
2026-07-01 19:47:04 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+window.addEventListener("scroll", () => {
 
+const nav = document.querySelector(".navbar");
+
+if(window.scrollY > 80){
+
+nav.classList.add("scrolled");
+
+}else{
+
+nav.classList.remove("scrolled");
+
+}
+
+});
