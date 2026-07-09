3b00e077eb685ee1daa6233a3a5eee263de2c2e6
chipkeezy <mcchipkeezy@gmail.com>
2026-07-09 19:25:33 +0300

    Update artworks.js

--- data/artworks.js
+++ data/artworks.js
@@ -11,32 +11,22 @@ price:"$850",
 status:"Available",
 featured:true
 },
-
+  
+{
+title:"The Last Ember",
+image:"images/paintings/feet.jpeg",
+story:"A quiet meditation on memory and disappearing light.",
+medium:"Oil on Canvas",
+size:"60 × 80 cm",
+year:"2026",
+price:"$850",
+status:"Available",
+featured:true
+},
+  
 {
 title:"Golden Silence",
 image:"images/paintings/boy.jpeg",
-story:"Light dissolving into atmosphere and memory.",
-medium:"Oil on Canvas",
-size:"60 × 80 cm",
-year:"2026",
-price:"$1200",
-status:"Available",
-featured:false
-},
-  
-title:"Feet That carried a Continent",
-image:"images/paintings/feet.jpeg",
-story:"Light dissolving into atmosphere and memory.",
-medium:"Oil on Canvas",
-size:"60 × 80 cm",
-year:"2026",
-price:"$1200",
-status:"Available",
-featured:false
-},
-
-title:"The woman king",
-image:"images/paintings/masai.jpeg",
 story:"Light dissolving into atmosphere and memory.",
 medium:"Oil on Canvas",
 size:"60 × 80 cm",
